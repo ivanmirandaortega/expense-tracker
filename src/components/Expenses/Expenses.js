06f653cd3523f9f5c92d317c1db8370b979c0fa3
Ivan Miranda Ortega <imirandaortega.pd@gmail.com>
2022-06-09 20:42:34 -0700

    added conditional to expesnes component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,6 +16,18 @@ function Expenses(props) {
         return expense.date.getFullYear().toString() === filteredYear
     })
 
+    let expesnesContent = <p>No expenses found</p>
+
+    if (filteredExpenses.length > 0) {
+        expesnesContent = filteredExpenses.map(expense =>
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />)
+    }
+
     return (
         <div>
             <Card className="expenses">
@@ -23,13 +35,7 @@ function Expenses(props) {
                     selected={filteredYear}
                     onChangeFilter={filterChangeHandler}
                 />
-                {filteredExpenses.map(expense =>
-                    <ExpenseItem
-                        key={expense.id}
-                        title={expense.title}
-                        amount={expense.amount}
-                        date={expense.date}
-                    />)}
+                {expesnesContent}
             </Card>
         </div>
     )
